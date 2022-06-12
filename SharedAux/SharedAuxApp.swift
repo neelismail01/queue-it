2426d7b7e349a1dd94d4a8ce9933b7f395e9bbbd
Neel Ismail <neelismail01@gmail.com>
2022-06-12 13:40:28 -0400

    Initial Commit

---
+++ SharedAux/SharedAuxApp.swift
@@ -0,0 +1,17 @@
+//
+//  SharedAuxApp.swift
+//  SharedAux
+//
+//  Created by Neel Ismail on 2022-06-12.
+//
+
+import SwiftUI
+
+@main
+struct SharedAuxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
