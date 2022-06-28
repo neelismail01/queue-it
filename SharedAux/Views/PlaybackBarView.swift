d0cce0f16620bd326ed24a69c0c4f00f78ed18dc
Neel Ismail <neelismail01@gmail.com>
2022-06-27 20:40:16 -0400

    creating song timeline + modularizing components

--- SharedAux/Views/PlaybackBarView.swift
+++ SharedAux/Views/PlaybackBarView.swift
@@ -10,82 +10,106 @@ import SwiftUI
 struct PlaybackBarView: View {
     
     @EnvironmentObject var viewModel: ViewModel
-    
-    var albumImage: some View {
-        let currentSong = viewModel.currentlyPlayingItem
-        
-        return Image(uiImage: (currentSong?.artwork?.image(at: CGSize(width: 50, height: 50))!)!)
-            .resizable()
-            .aspectRatio(contentMode: .fit)
-            .frame(width: 50, height: 50, alignment: .center)
-            .clipShape(RoundedRectangle(cornerRadius: 10))
-            .padding()
-    }
+    @State var songTime = 0.0
     
     var albumImagePlaceholder: some View {
         Image(systemName: "music.note")
-            .frame(width: 50, height: 50, alignment: .center)
+            .frame(width: 40, height: 40, alignment: .center)
             .background(.gray)
             .clipShape(RoundedRectangle(cornerRadius: 10))
-            .padding()
+    }
+    
+    var songDetails: some View {
+        let currentSong = viewModel.currentlyPlayingItem
+        return HStack {
+            if let song = currentSong {
+                if let songArtwork = song.artwork?.image(at: CGSize(width: 40, height: 40)) {
+                    Image(uiImage: songArtwork)
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 40, height: 40, alignment: .center)
+                        .clipShape(RoundedRectangle(cornerRadius: 10))
+                } else {
+                    albumImagePlaceholder
+                }
+                VStack(alignment: .leading) {
+                    Text(currentSong?.title ?? "Unknown Song")
+                        .font(.system(size: 14, weight: .semibold))
+                    Text(currentSong?.artist ?? "Unknown Artist")
+                        .font(.system(size: 14, weight: .light))
+                        .foregroundColor(.gray)
+                }
+                .padding(.leading, 5)
+                .padding(.trailing)
+            } else {
+                albumImagePlaceholder
+                Text("Not Playing")
+                    .font(.system(size: 18, weight: .semibold))
+                    .foregroundColor(.gray)
+                    .padding(.leading, 5)
+                    .padding(.trailing)
+            }
+        }
+    }
+    
+    var playPauseButton: some View {
+        let currentSong = viewModel.currentlyPlayingItem
+        return HStack {
+            if currentSong == nil {
+                Image(systemName: "play.fill")
+                    .font(.system(size: 22))
+                    .foregroundColor(.gray)
+            } else if viewModel.isSongPlaying {
+                Button {
+                    viewModel.musicPlayer.pause()
+                    viewModel.isSongPlaying = false
+                } label: {
+                    Image(systemName: "pause.fill")
+                        .font(.system(size: 22))
+                }
+            } else {
+                Button {
+                    viewModel.musicPlayer.play()
+                    viewModel.isSongPlaying = true
+                } label: {
+                    Image(systemName: "play.fill")
+                        .font(.system(size: 22))
+                }
+            }
+        }
+    }
+    
+    var songTimeline: some View {
+        let currentSong = viewModel.currentlyPlayingItem
+        return HStack {
+            if let song = currentSong {
+                Rectangle()
+                    .fill(.purple)
+                    .frame(width: UIScreen.main.bounds.size.width * (songTime / song.playbackDuration),
+                           height: 3)
+            }
+            Rectangle()
+                .fill(.white)
+                .frame(height: 3)
+        }
+        .background(.white)
     }
     
     var body: some View {
-        let currentSong = viewModel.currentlyPlayingItem
-
-        VStack() {
+        VStack {
             Spacer(minLength: 0)
-            HStack {
-                if currentSong != nil {
-                    if currentSong?.artwork != nil {
-                        albumImage
-                    } else {
-                        albumImagePlaceholder
-                    }
-                    VStack(alignment: .leading) {
-                        Text(currentSong?.title ?? "Unknown Song")
-                            .font(.system(size: 16, weight: .semibold))
-                        Text(currentSong?.artist ?? "Unknown Artist")
-                            .font(.system(size: 14, weight: .light))
-                            .foregroundColor(.gray)
-                    }
-                } else {
-                    albumImagePlaceholder
-                    Text("Not Playing")
-                        .font(.system(size: 20, weight: .semibold))
-                        .foregroundColor(.gray)
-                }
-                
+            VStack {
+                songTimeline
                 Spacer()
-                
-                if currentSong == nil {
-                    Image(systemName: "play.fill")
-                        .padding()
-                        .font(.system(size: 22))
-                        .foregroundColor(.gray)
-                } else if viewModel.isSongPlaying {
-                    Button {
-                        print("supposed to be true: \(viewModel.isSongPlaying)")
-                        viewModel.musicPlayer.pause()
-                        viewModel.isSongPlaying = false
-                    } label: {
-                        Image(systemName: "pause.fill")
-                            .padding()
-                            .font(.system(size: 22))
-                    }
-                } else {
-                    Button {
-                        print("supposed to be false: \(viewModel.isSongPlaying)")
-                        viewModel.musicPlayer.play()
-                        viewModel.isSongPlaying = true
-                    } label: {
-                        Image(systemName: "play.fill")
-                            .padding()
-                            .font(.system(size: 22))
-                    }
+                HStack {
+                    songDetails
+                    Spacer()
+                    playPauseButton
                 }
+                .padding(.leading)
+                .padding(.trailing)
             }
-            .frame(width: UIScreen.main.bounds.size.width, height: 65, alignment: .leading)
+            .frame(width: UIScreen.main.bounds.size.width, height: 60)
             .foregroundColor(.black)
             .background(.white)
             .onTapGesture {
@@ -94,6 +118,7 @@ struct PlaybackBarView: View {
         }
         .onReceive(viewModel.timer) { _ in
             viewModel.checkCurrentlyPlayingSong()
+            songTime = viewModel.musicPlayer.currentPlaybackTime
         }
     }
 }
