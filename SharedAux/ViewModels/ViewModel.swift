ae4bc416cb849773b579c3a0179751fe36bce16f
Neel Ismail <neelismail01@gmail.com>
2022-07-01 01:02:41 -0400

    creating methods for playing, skipping, and adding music in the queue

--- SharedAux/ViewModels/ViewModel.swift
+++ SharedAux/ViewModels/ViewModel.swift
@@ -17,11 +17,13 @@ class ViewModel: ObservableObject {
     
     @Published var songSearchResults: [Song] = []
     
+    var queue: [String] = []
+    var currentSongQueueIndex: Int = -1
+    
     @Published var currentlyPlayingItem: MPMediaItem?
     @Published var isSongPlaying = false
     
     @Published var isPlayerViewPresented = false
-    
     
     
     func loginWithAppleMusic(_ status: MusicAuthorization.Status) {
@@ -44,7 +46,30 @@ class ViewModel: ObservableObject {
     
     func checkCurrentlyPlayingSong() {
         if let currentSong = musicPlayer.nowPlayingItem {
-            currentlyPlayingItem = currentSong
+            if currentSong.playbackDuration - musicPlayer.currentPlaybackTime < 1 {
+                playNextSong()
+            } else {
+                currentlyPlayingItem = currentSong
+            }
         }
+    }
+    
+    func addSongToQueue(_ songId: String) {
+        queue.append(songId)
+        if musicPlayer.nowPlayingItem == nil {
+            playNextSong()
+        }
+    }
+    
+    func playNextSong() {
+        if currentSongQueueIndex < queue.count - 1 {
+            currentSongQueueIndex += 1
+        } else {
+            currentSongQueueIndex = 0
+        }
+        musicPlayer.setQueue(with: [queue[currentSongQueueIndex]])
+        musicPlayer.play()
+        currentlyPlayingItem = musicPlayer.nowPlayingItem
+        isSongPlaying = true
     }
 }
