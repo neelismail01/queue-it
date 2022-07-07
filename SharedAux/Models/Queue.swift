a7bf6d8ab9c833f2346c3e3b42d9338d844731dc
Neel Ismail <neelismail01@gmail.com>
2022-07-07 18:52:09 -0400

    adding song details to model

--- SharedAux/Models/Queue.swift
+++ SharedAux/Models/Queue.swift
@@ -7,11 +7,21 @@
 
 import Foundation
 import FirebaseFirestoreSwift
+import MusicKit
 
 struct Queue: Codable {
     @DocumentID var id: String?
     var name: String
     var joinCode: String
-    var songs: [String]
+    var songAdditions: [SongAddition]
     var active: Bool
+}
+
+struct SongAddition: Identifiable, Codable {
+    var id: String = UUID().uuidString
+    var addedBy: String
+    var songId: String
+    var songName: String
+    var songArtist: String
+    var songArtworkUrl: String
 }
