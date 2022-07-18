8559de55a75b1eaa208b1182485152c5daf8ecb3
Neel Ismail <neelismail01@gmail.com>
2022-07-18 16:33:34 -0400

    integrating sdwebimage + adding padding around tracks

--- SharedAux/Views/ArtistTopSongsView.swift
+++ SharedAux/Views/ArtistTopSongsView.swift
@@ -11,23 +11,13 @@ import MusicKit
 struct ArtistTopSongsView: View {
     let topSongs: [Song]
     var body: some View {
-        ForEach(topSongs) { song in
-            HStack {
-                AsyncImage(url: song.artwork?.url(width: 50, height: 50))
-                    .frame(width: 50, height: 50, alignment: .center)
-                    .cornerRadius(5)
-                VStack(alignment: .leading) {
-                    Text(song.title)
-                        .font(.system(size: 16, weight: .semibold))
-                        .lineLimit(1)
-                    Text("Song - \(song.artistName)")
-                        .font(.system(size: 14, weight: .light))
-                        .lineLimit(1)
-                        .foregroundColor(.gray)
-                }
-                Spacer()
-                Image(systemName: "plus.circle")
+        VStack {
+            ForEach(topSongs) { song in
+                TrackRow(song: song)
+                    .padding(.horizontal)
             }
         }
+        .padding(.vertical)
+        .navigationTitle("Top Songs")
     }
 }
