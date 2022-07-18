54b95370c80f4bfb8c94953307967853d6e3d794
Neel Ismail <neelismail01@gmail.com>
2022-07-18 16:33:03 -0400

    creating ui for top songs and albums + integrating sdwebimage

--- SharedAux/Views/ArtistView.swift
+++ SharedAux/Views/ArtistView.swift
@@ -7,6 +7,7 @@
 
 import SwiftUI
 import MusicKit
+import SDWebImageSwiftUI
 
 enum ViewState {
     case loading
@@ -20,67 +21,105 @@ struct ArtistView: View {
     @State var detailedArtistInfo: Artist?
     @State var state: ViewState = .loading
     let artist: Artist
+    let columns = [
+        GridItem(.flexible()),
+        GridItem(.flexible()),
+    ]
     
-    var body: some View {
+    var loading: some View {
+        return Text("Loading...")
+    }
+    
+    var topSongsSection: some View {
+        let topSongs = detailedArtistInfo?.topSongs ?? []
+        return VStack {
+            HStack {
+                Text("Top Songs")
+                    .font(.system(size: 22).bold())
+                Spacer()
+                NavigationLink {
+                    ArtistTopSongsView(topSongs: Array(detailedArtistInfo!.topSongs ?? []))
+                } label: {
+                    Text("View All")
+                        .font(.system(size: 16))
+                }
+                
+            }
+            ForEach(topSongs.count > 4 ? Array(topSongs[...4]) : Array(topSongs)) { song in
+                TrackRow(song: song)
+            }
+        }
+        .padding(.bottom, 15)
+    }
+    
+    var albumsSection: some View {
+        
+        let albums = detailedArtistInfo?.albums ?? []
         let dateFormatter = DateFormatter()
         dateFormatter.dateFormat = "yyyy"
+        
         return VStack {
-            if state == .loading {
-                Text("Loading...")
-            } else {
-                ScrollView {
-                    if let topSongs = detailedArtistInfo?.topSongs {
-                        HStack {
-                            Text("Top Songs")
-                                .font(.system(size: 16, weight: .semibold))
-                            Spacer()
-                            NavigationLink {
-                                ArtistTopSongsView(topSongs: Array(detailedArtistInfo!.topSongs ?? []))
-                            } label: {
-                                Text("View All")
-                            }
+            HStack {
+                Text("Albums")
+                    .font(.system(size: 22).bold())
+                Spacer()
+            }
 
-                        }
-                        ForEach(topSongs.count > 5 ? Array(topSongs[...5]) : Array(topSongs)) { song in
-                            HStack {
-                                AsyncImage(url: song.artwork?.url(width: 50, height: 50))
-                                    .frame(width: 50, height: 50, alignment: .center)
-                                    .cornerRadius(5)
-                                VStack(alignment: .leading) {
-                                    Text(song.title)
-                                        .font(.system(size: 16, weight: .semibold))
-                                        .lineLimit(1)
-                                    Text("Song - \(song.artistName)")
-                                        .font(.system(size: 14, weight: .light))
-                                        .lineLimit(1)
-                                        .foregroundColor(.gray)
+            LazyVGrid(columns: columns) {
+                ForEach(albums) { album in
+                    NavigationLink {
+                        AlbumView(album: album)
+                    } label: {
+                        VStack(alignment: .leading) {
+                            WebImage(url: album.artwork?.url(width: 300, height: 300))
+                                .placeholder(
+                                    Image(systemName: "music.note")
+                                )
+                                .resizable()
+                                .frame(width: 150, height: 150)
+                                .cornerRadius(5)
+                            HStack() {
+                                Text(album.title)
+                                    .font(.system(size: 14, weight: .semibold))
+                                    .foregroundColor(Color(UIColor.label))
+                                    .lineLimit(1)
+                                if album.contentRating == .explicit {
+                                    Text("E")
+                                        .frame(width: 15, height: 15)
+                                        .font(.system(size: 8))
+                                        .foregroundColor(Color(UIColor.systemBackground))
+                                        .background(Color(UIColor.systemGray))
+                                        .cornerRadius(2.5)
                                 }
                                 Spacer()
-                                Image(systemName: "plus.circle")
                             }
-                        }
-                    }
-                    ScrollView(.horizontal) {
-                        HStack {
-                            ForEach(artist.albums ?? []) { album in
-                                NavigationLink {
-                                    AlbumView(album: album)
-                                } label: {
-                                    VStack {
-                                        AsyncImage(url: album.artwork?.url(width: 150, height: 150))
-                                            .frame(width: 150, height: 150)
-                                            .cornerRadius(10)
-                                        Text(album.title)
-                                        if let releaseDate = album.releaseDate {
-                                            Text(dateFormatter.string(from: releaseDate))
-                                        }
-                                    }
-                                }
+                            if let releaseDate = album.releaseDate {
+                                Text(dateFormatter.string(from: releaseDate))
+                                    .font(.system(size: 14))
+                                    .foregroundColor(Color(UIColor.secondaryLabel))
                             }
                         }
                     }
                 }
-                
+            }
+        }
+    }
+    
+    var artistInformation: some View {
+        ScrollView {
+            topSongsSection
+                .padding()
+            albumsSection
+                .padding()
+        }
+    }
+    
+    var body: some View {
+        VStack {
+            if state == .loading {
+                loading
+            } else {
+                artistInformation
             }
         }
         .onAppear {
